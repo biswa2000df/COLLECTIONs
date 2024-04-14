52bb27f9de7a9103120010ff070277385838e5f4
biswajit <biswajitsahookanha11@gmail.com>
2024-04-14 15:38:57 +0530

    println statement

--- src/main/java/Collections1/ArrayListDemo.java
+++ src/main/java/Collections1/ArrayListDemo.java
@@ -16,11 +16,11 @@ public class ArrayListDemo {
 		al.add(10.8);
 		al.add('A');
 		al.add(true);
-		System.out.println(al);
-		System.out.println("The size of the ArrayList ="+al.size());
-		System.out.println("Adding new element are: "+al.add(3));
-		System.out.println(al.get(3));
-		System.out.println(al);
+		System.out.println(al);   //[100, Welcome, 10.8, A, true]
+		System.out.println("The size of the ArrayList ="+al.size());    //5
+		System.out.println("Adding new element are: "+al.add(3));     //Adding new element are: true
+		System.out.println(al.get(3));   //A
+		System.out.println(al);				//[100, Welcome, 10.8, A, true, 3]
 		//System.out.println(al.set(4, 200));
 		//System.out.println(al);
 		System.out.println(al.contains(20));
@@ -39,14 +39,14 @@ public class ArrayListDemo {
 		for(i=0;i<al.size();i++)
 		{
 			
-			System.out.println(al.get(i));
+			System.out.print(al.get(i) + " ");
 		}
 		//2.For each Loop
 		System.out.println("Reading element using foreach loop");
 		for(Object e:al)
 		{
 			
-			System.out.println(e);
+			System.out.print(e + " ");
 		}
 		
 		//3.Iterator();
@@ -56,7 +56,7 @@ public class ArrayListDemo {
 			
 			while(it.hasNext())
 			{
-				System.out.println(it.next());
+				System.out.print(it.next() + " ");
 			}
 		}
 		
